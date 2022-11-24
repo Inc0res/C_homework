fbced58efc0801211943a5523e9c6e5195f9699a
incores <9808885588@mail.ru>
2022-11-24 17:43:30 +0300

    task 2

---
+++ task 2/Program.cs
@@ -0,0 +1,13 @@
+﻿Console.Clear();
+Console.Write("Введите число: ");
+int a = Convert.ToInt32(Console.ReadLine());
+Console.Write("Введите число 2: ");
+int b = Convert.ToInt32(Console.ReadLine());
+
+int max = a;
+int min = b;
+if (b>a)max = b;
+if (a<b)min = a;
+Console.WriteLine(max + " Максимум");
+
+Console.WriteLine(min + " Минимум");
